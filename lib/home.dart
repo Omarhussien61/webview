db337c56861fe293b3d9786c3e533005b0e661e0
omarhussien61@gmail.com <Oo23119950>
2021-04-27 15:54:27 +0200

    g

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 
 class Home extends StatefulWidget {
@@ -17,22 +19,30 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
-
     super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
+
 
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-      appBar: AppBar(title: Text('Flespi'),centerTitle: true,),
-      url: "https://flespi.io/#/",
-      hidden: false,
-      initialChild: Container(
-        child: const Center(
-          child: Text('Waiting.....'),
-        ),
+    return Scaffold(
+        appBar: AppBar(title: Text('Flespi'),centerTitle: true,),
+    body: WebView(
+        initialUrl: 'https://flutter.dev',
       ),
     );
+    //   WebviewScaffold(
+    //   appBar: AppBar(title: Text('Flespi'),centerTitle: true,),
+    //   url: "https://flespi.io/#/",
+    //   hidden: false,
+    //   initialChild: Container(
+    //     child: const Center(
+    //       child: Text('Waiting.....'),
+    //     ),
+    //   ),
+    // );
   }
 
   @override
